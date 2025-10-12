903ab72dc5f256478447254dfeacd323d8d0aafe
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-10-12 14:22:27 +0530

    Import correct Authprovider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider } from './contexts/ThemeContext.tsx' 
+import { ThemeProvider } from './contexts/ThemeContext'
+import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider> {/* Wrap with ThemeProvider */}
-      <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
